f69ab54b2d116e997fce58b09e2f38a5019f4fa9
Ashhad Rasool <ashhad.rasool@qisstpay.com>
2022-12-01 17:37:02 +0500

    HMB status api + dynamic userid/pass

---
+++ src/main/java/com/qisstpay/lendingservice/dto/hmb/request/GetTransactionStatusRequestDto.java
@@ -0,0 +1,22 @@
+package com.qisstpay.lendingservice.dto.hmb.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GetTransactionStatusRequestDto {
+    @JsonProperty("MakerID")
+    private String makerID;
+    @JsonProperty("REFNO")
+    private String refNo;
+    @JsonProperty("Stan")
+    private String stan;
+    @JsonProperty("DateTime")
+    private String dateTime;
+}
--- src/main/java/com/qisstpay/lendingservice/service/HMBPaymentService.java
+++ src/main/java/com/qisstpay/lendingservice/service/HMBPaymentService.java
@@ -1,8 +1,10 @@
 package com.qisstpay.lendingservice.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.qisstpay.lendingservice.dto.hmb.request.GetTransactionStatusRequestDto;
 import com.qisstpay.lendingservice.dto.hmb.request.SubmitTransactionRequestDto;
 import com.qisstpay.lendingservice.dto.hmb.response.GetTokenResponseDto;
+import com.qisstpay.lendingservice.dto.hmb.response.GetTransactionStatusResponseDto;
 import com.qisstpay.lendingservice.dto.hmb.response.SubmitTransactionResponseDto;
 
 public interface HMBPaymentService {
@@ -10,5 +12,5 @@ public interface HMBPaymentService {
     GetTokenResponseDto getToken();
     SubmitTransactionResponseDto submitIFTTransaction(String token, SubmitTransactionRequestDto submitTransactionRequestDto);
     SubmitTransactionResponseDto submitIBFTTransaction(String token, SubmitTransactionRequestDto submitTransactionRequestDto) throws Exception;
-
+    GetTransactionStatusResponseDto getStatus(String authToken, GetTransactionStatusRequestDto getTransactionStatusRequestDto) throws Exception;
 }
